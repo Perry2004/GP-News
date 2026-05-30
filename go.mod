727fac98dbacb20597a78138f259e9e458a71ce0
Perry Zhu <perryzhu2004@outlook.com>
2026-05-29 22:26:12 -0700

    feat: go start config

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module gpnews
 
 go 1.26.3
+
+require (
+	github.com/caarlos0/env/v11 v11.4.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
+
+type config struct {
+	LogLevel string `env:"LOG_LEVEL" envDefault:"info"` // debug, info, warn, error
+}
 
 func main() {
-	fmt.Println("Hello, GPNews!")
-
 	// [TODO] Load config and credentials
+	envName := loadEnv()
+	cfg, err := env.ParseAs[config]()
+	if err != nil {
+		panic(err)
+	}
+
+	configureLogger(cfg)
+	slog.Debug("GP-News configuration loaded", "environment", envName, "config", cfg)
+	slog.Info("Starting GP-News")
 
 	// [TODO] Fetch Yahoo Finance
 
@@ -24,4 +43,41 @@ func main() {
 	// [TODO] Render email template
 
 	// [TODO] Send email
+}
+
+func loadEnv() string {
+	envName := os.Getenv("ENVIRONMENT")
+	switch strings.ToLower(strings.TrimSpace(envName)) {
+	case "", "dev":
+		if err := godotenv.Load(); err != nil {
+			panic(fmt.Errorf("failed to load .env file: %w", err))
+		}
+	case "prod":
+		// Skip loading .env
+	default:
+		panic(fmt.Errorf("invalid environment %q: expected dev or prod", envName))
+	}
+
+	return envName
+}
+
+func configureLogger(cfg config) {
+	var logLevel slog.Level
+	switch strings.ToLower(strings.TrimSpace(cfg.LogLevel)) {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "", "info":
+		logLevel = slog.LevelInfo
+	case "warn":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	default:
+		panic(fmt.Errorf("invalid log level %q: expected debug, info, warn, or error", cfg.LogLevel))
+	}
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	}))
+	slog.SetDefault(logger)
 }
